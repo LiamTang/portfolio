c34aa0da40947fb8f7a8a7d02e70de3400c64e06
Liam Tang <tangwangjun123@gmail.com>
2020-08-25 22:51:02 +1000

    resolve majority of issues of eslint format, add project calendar

--- js/script.js
+++ js/script.js
@@ -15,7 +15,10 @@ document.addEventListener('DOMContentLoaded', () => {
       charCount += 1;
       setTimeout(typing, 100);
     } else if (charCount >= array[index].length) {
-      const delChar = `${array[index].substring(-array[index].length, charCount)}|`;
+      const delChar = `${array[index].substring(
+        -array[index].length,
+        charCount,
+      )}|`;
       pLocation.innerHTML = delChar;
       charCount -= 1;
       setTimeout(deleteText, 500);
@@ -24,7 +27,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
   function deleteText() {
     if (charCount >= 0) {
-      const delChar = `${array[index].substring(-array[index].length, charCount)}|`;
+      const delChar = `${array[index].substring(
+        -array[index].length,
+        charCount,
+      )}|`;
       pLocation.innerHTML = delChar;
       charCount -= 1;
       setTimeout(deleteText, 50);
@@ -48,7 +54,13 @@ document.addEventListener('DOMContentLoaded', () => {
       sections[e.id] = e.offsetTop;
     });
     const eachSectionHeight = Object.values(sections);
-    const eachSectionName = ['homepage', 'about-page', 'works-page', 'education-page', 'contact-page'];
+    const eachSectionName = [
+      'homepage',
+      'about-page',
+      'works-page',
+      'education-page',
+      'contact-page',
+    ];
     if (scrollPosition < homepage.offsetHeight - 100) {
       document.querySelector('.nav-bar').classList.remove('show-nav');
       document.querySelector('.back-top').classList.remove('show-back-top');
@@ -63,81 +75,35 @@ document.addEventListener('DOMContentLoaded', () => {
             .setAttribute('class', 'active');
         }
       }
-      // for (const i in sections) {
-      //   if (sections[i] <= scrollPosition) {
-      //     document.querySelector('.back-top').classList.add('show-back-top');
-      //     document.querySelector('.active').setAttribute('class', ' ');
-      //     document.querySelector('.nav-bar').classList.add('show-nav');
-      //     navLink
-      //       .querySelector(`a[href*=${i}]`)
-      //       .setAttribute('class', 'active');
-      //   }
-      // }
     }
   };
-});
 
-/** dark mode setting */
-const setDarkMode = () => {
-  const { body } = document;
-  body.classList.toggle('dark-mode');
-  const title = document.querySelector('.title');
-  const image = title.querySelector('img');
-  if (image.alt.match('light')) {
-    image.src = './asset/image/moon.svg';
-    image.alt = 'moon';
-  } else {
-    image.src = './asset/image/sun.svg';
-    image.alt = 'light';
-  }
-};
+  /** dark mode setting */
+  document.querySelector('.light-icon').addEventListener('click', () => {
+    const { body } = document;
+    body.classList.toggle('dark-mode');
+    const title = document.querySelector('.title');
+    const image = title.querySelector('img');
+    if (image.alt.match('light')) {
+      image.src = './asset/image/moon.svg';
+      image.alt = 'moon';
+    } else {
+      image.src = './asset/image/sun.svg';
+      image.alt = 'light';
+    }
+  });
 
-/** set slide of work */
-const setSlide = (id) => {
-  const weatherProject = document.querySelector('.slider-weather-project');
-  const pizzaProject = document.querySelector('.slider-pizza-project');
-  const calculatorProject = document.querySelector(
-    '.slider-calculator-project',
-  );
+  /** set navigation toggle in the small media screen */
+  document.querySelector('.nav-toggle').addEventListener('click', () => {
+    const navLink = document.querySelector('.nav-link');
+    navLink.classList.toggle('nav-link_active');
 
-  if (id === 1) {
-    weatherProject.classList.add('active-slide');
-    pizzaProject.classList.remove('active-slide');
-    calculatorProject.classList.remove('active-slide');
-    weatherProject.classList.remove('hide-slide');
-  }
-  if (id === 2) {
-    pizzaProject.classList.remove('hide-slide');
-    weatherProject.classList.add('hide-slide');
-    calculatorProject.classList.add('hide-slide');
-    pizzaProject.classList.add('active-slide');
-    calculatorProject.classList.remove('active-slide');
-    weatherProject.classList.remove('active-slide');
-  }
-  if (id === 3) {
-    calculatorProject.classList.remove('hide-slide');
-    calculatorProject.classList.add('active-slide');
-    weatherProject.classList.remove('active-slide');
-    pizzaProject.classList.remove('active-slide');
-    weatherProject.classList.add('hide-slide');
-    pizzaProject.classList.add('hide-slide');
-  }
-};
+    const navBar = document.querySelector('.nav-bar');
+    navBar.classList.toggle('nav-toggle_active');
+  });
 
-/** set navigation toggle in the small media screen */
-const toggle = () => {
-  const navLink = document.querySelector('.nav-link');
-  navLink.classList.toggle('nav-link_active');
-
-  const navBar = document.querySelector('.nav-bar');
-  navBar.classList.toggle('nav-toggle_active');
-};
-
-/** set email validation */
-const myForm = () => {
+  /** set email validation */
   document.querySelector('.submit').addEventListener('click', (e) => {
-    // const name = document.forms.contactForm.fullName.value;
-    // const email = document.forms.contactForm.email.value;
     const subject = document.forms.contactForm.subject.value;
     const message = document.forms.contactForm.message.value;
     if (subject.length <= 2) {
@@ -147,4 +113,58 @@ const myForm = () => {
       e.preventDefault();
     }
   });
-};
+
+  /** set slide animation of work lists */
+  const weatherProject = document.querySelector('.slider-weather-project');
+  const pizzaProject = document.querySelector('.slider-pizza-project');
+  const calculatorProject = document.querySelector(
+    '.slider-calculator-project',
+  );
+  const calendarProject = document.querySelector(
+    '.slider-calendar-project',
+  );
+  document.querySelector('.list1').addEventListener('click', () => {
+    weatherProject.classList.add('active-slide');
+    weatherProject.classList.remove('hide-slide');
+
+    pizzaProject.classList.remove('active-slide');
+    calculatorProject.classList.remove('active-slide');
+    calendarProject.classList.remove('active-slide');
+  });
+
+  document.querySelector('.list2').addEventListener('click', () => {
+    pizzaProject.classList.add('active-slide');
+    pizzaProject.classList.remove('hide-slide');
+
+    calculatorProject.classList.add('hide-slide');
+    calculatorProject.classList.remove('active-slide');
+    weatherProject.classList.add('hide-slide');
+    weatherProject.classList.remove('active-slide');
+    calendarProject.classList.add('hide-slide');
+    calendarProject.classList.remove('active-slide');
+  });
+
+  document.querySelector('.list3').addEventListener('click', () => {
+    calculatorProject.classList.remove('hide-slide');
+    calculatorProject.classList.add('active-slide');
+
+    weatherProject.classList.remove('active-slide');
+    weatherProject.classList.add('hide-slide');
+    pizzaProject.classList.remove('active-slide');
+    pizzaProject.classList.add('hide-slide');
+    calendarProject.classList.add('hide-slide');
+    calendarProject.classList.remove('active-slide');
+  });
+
+  document.querySelector('.list4').addEventListener('click', () => {
+    calendarProject.classList.add('active-slide');
+    calendarProject.classList.remove('hide-slide');
+
+    calculatorProject.classList.add('hide-slide');
+    calculatorProject.classList.remove('active-slide');
+    weatherProject.classList.remove('active-slide');
+    weatherProject.classList.add('hide-slide');
+    pizzaProject.classList.add('hide-slide');
+    pizzaProject.classList.remove('active-slide');
+  });
+});
